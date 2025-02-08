c8ea3fcdf849968728db78416b7b083206b99b56
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2025-02-08 15:22:29 -0300

    criando lista de questoes

---
+++ src/Questoes.tsx
@@ -0,0 +1,14 @@
+export const Questoes = [
+  {
+    id: 1,
+    titulo: "O que é o HTML?",
+    exemplo: "bla bla",
+    envios: [],
+  },
+  {
+    id: 2,
+    titulo: "Para que serve o CSS?",
+    exemplo: "bla bla",
+    envios: [],
+  },
+];
