dac55034da9a0765feb2e54d89d3f5180efbc167
ianfefe <felix.ian@estudante.ufjf.br>
2025-02-08 18:45:03 -0300

    adiciona estado para as respostas

--- src/QuestaoItem.tsx
+++ src/QuestaoItem.tsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
 
-interface questao{
-
-  id: number;
-  titulo: string;
-  resposta: string;
+interface questaoInterface {
+  pergunta: String;
+  exemplo: String
 }
 
-export default function QuestaoItem({questao}: {questao: questao}) {
-  return <li >{questao.titulo}</li>;
+export default function QuestaoItem({pergunta, exemplo}: questaoInterface) {
+  const [resposta, setResposta] = useState('');
+  const [bloqueado, setBloqueado] = useState(false);
+  
+  const validaResposta = (e: React.FormEvent) => {
+    resposta === ' ' ? alert("Resposta vazia") : setBloqueado(true);
+    e.preventDefault();
+  }
+
+  function editarResposta(){
+    setBloqueado(false);
+  }
+
+  return <>
+  <div className="pergunta">{pergunta}</div>
+  <div className="exemplo">Ex: {exemplo}</div>
+    <form onSubmit={validaResposta}>
+      <input
+        type="text"
+        placeholder="Digite sua resposta aqui..."
+        value={resposta}
+        onChange={(e) => setResposta(e.target.value)}
+        disabled={bloqueado}
+      />
+      <button type="submit">Enviar</button>
+      <button type="button" onClick={editarResposta}>Editar</button>
+    </form>  
+  </>
 }
