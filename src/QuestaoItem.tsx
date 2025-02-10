0c89f02ad284362e05138c1153aaf086817b9834
ianfefe <felix.ian@estudante.ufjf.br>
2025-02-09 23:28:03 -0300

    agrupa campo de resposta e botoes

--- src/QuestaoItem.tsx
+++ src/QuestaoItem.tsx
@@ -12,8 +12,9 @@ interface Estado {
 }
 
 type Action =
-  | { type: "setResposta"; payload: string }
-  | { type: "bloquear" | "editar" };
+  | { type: "setResposta"; texto: string }
+  | { type: "bloquear"}
+  | { type: "editar"};
 
 const estadoInicial: Estado = {
   resposta: "",
@@ -25,7 +26,7 @@ function reducer(estado: Estado, action: Action):Estado {
     default:
       return estado;
     case "setResposta":
-      return { ...estado, resposta: action.payload };
+      return { ...estado, resposta: action.texto };
     case "bloquear":
       return { ...estado, bloqueado: true };
     case "editar":
@@ -48,17 +49,19 @@ export default function QuestaoItem({ pergunta, exemplo }: questaoInterface) {
     <>
       <div className="pergunta">{pergunta}</div>
       <div className="exemplo">Ex: {exemplo}</div>
-      <input
-        type="text"
-        placeholder="Digite sua resposta aqui..."
-        value={estado.resposta}
-        onChange={(e) =>
-          dispatch({ type: "setResposta", payload: e.target.value })
-        }
-        disabled={estado.bloqueado}
-      />
-      <button type="submit" onClick={validaResposta}>Enviar</button>
-      <button type="button" onClick={() => dispatch({type: 'editar'})}>Editar</button>
+      <div className="campo_resposta">
+        <input
+          type="text"
+          placeholder="Digite sua resposta aqui..."
+          value={estado.resposta}
+          onChange={(e) =>
+            dispatch({ type: "setResposta", texto: e.target.value })
+          }
+          disabled={estado.bloqueado}
+        />
+        <button type="submit" onClick={validaResposta}>Enviar</button>
+        <button type="button" onClick={() => dispatch({type: 'editar'})}>Editar</button>
+      </div>
     </>
   );
 }
